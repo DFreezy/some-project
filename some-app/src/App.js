a7be5117ae6e8888f784e480d83a6582e28a8b35
name <duwaynefrieslaar23@gmail.com>
2024-11-23 18:25:56 +0200

    changed files

---
+++ some-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <h1>XXX</h1>
+  <h2>Find out how Lisa made up for stealing Ronnie's wallet</h2>
+  <p>Ronnie approached Lisa while she's reading a book</p>
+    </div>
+  );
+}
+
+export default App;
